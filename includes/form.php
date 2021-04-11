74bd212df35e0b70b69ed83494b891b1a97efaf1
Matthew Hull <matt@hullsite.com>
2021-04-11 11:19:38 -0400

    Updated questions

--- includes/form.php
+++ includes/form.php
@@ -121,11 +121,12 @@ $lastBuilding = getLastBuilding($_SESSION["userName"]);
                             </label>
                         </div>
                     </div>
+                    <!--
                     <div class="row">
                         <p style=color:red><em>For domestic travel, if you have been fully vaccinated and/or are previously diagnosed with 
                         laboratory confirmed COVID-19 and since recovered, please answer no.</em></p>
                     </div>
-                </div>
+                </div> -->
                     <!-- Next & Previous Buttons -->
                 <div style="overflow:auto;">
                     <div style="float:right;">
